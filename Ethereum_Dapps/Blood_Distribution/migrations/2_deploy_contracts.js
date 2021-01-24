accd9c9d5a23f8ee919370efa511ef49515435bf
anzinius <anzinius@gmail.com>
2021-01-24 23:32:46 +0900

    Deploy contracts

--- Ethereum_Dapps/Blood_Distribution/migrations/2_deploy_contracts.js
+++ Ethereum_Dapps/Blood_Distribution/migrations/2_deploy_contracts.js
@@ -1,6 +1,15 @@
 // mycontract 불러오고 노드 배포
-const MyContract = artifacts.require("./MyContract.sol");
+const Blood1 = artifacts.require("./Blood1.sol");
+const Blood2 = artifacts.require("./Blood2.sol");
+const Blood3 = artifacts.require("./Blood3.sol");
+const Blood4 = artifacts.require("./Blood4.sol");
+const Blood5 = artifacts.require("./Blood5.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(MyContract); // 이더리움 가상머신에 배포
+  deployer.deploy(Blood1); // 이더리움 가상머신에 배포
+  deployer.deploy(Blood2); // 이더리움 가상머신에 배포
+  deployer.deploy(Blood3); // 이더리움 가상머신에 배포
+  deployer.deploy(Blood4); // 이더리움 가상머신에 배포
+  deployer.deploy(Blood5); // 이더리움 가상머신에 배포
+
 };
